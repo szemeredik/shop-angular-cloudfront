6755ecf8627141ec556c85df7c732c5c71386fd8
Krisztian Szemeredi <krisztian_szemeredi@epam.com>
2024-06-15 18:44:16 +0200

    new api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://mllq8cqyhk.execute-api.eu-central-1.amazonaws.com/dev',
-    order: 'https://mllq8cqyhk.execute-api.eu-central-1.amazonaws.com/dev',
-    import: 'https://mllq8cqyhk.execute-api.eu-central-1.amazonaws.com/dev',
-    bff: 'https://mllq8cqyhk.execute-api.eu-central-1.amazonaws.com/dev',
-    cart: 'https://mllq8cqyhk.execute-api.eu-central-1.amazonaws.com/dev',
+    product: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
+    order: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
+    import: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
+    bff: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
+    cart: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
