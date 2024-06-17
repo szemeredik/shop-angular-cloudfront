f2edb9e999e261ba27e7ef0c9f99cbc983862d8c
Krisztian Szemeredi <krisztian_szemeredi@epam.com>
2024-06-17 17:09:31 +0200

    file upload integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
     order: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
-    import: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
+    import: 'https://p2th1l7x55.execute-api.eu-central-1.amazonaws.com/dev',
     bff: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
     cart: 'https://edeupbwwm0.execute-api.eu-central-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
