2bc4fe5f1a549b9a5b163970a5d426bdf71d2c03
Krisztian Szemeredi <krisztian_szemeredi@epam.com>
2024-06-27 21:46:57 +0200

    API endpoints changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://1k2a51u50g.execute-api.eu-central-1.amazonaws.com/dev',
-    order: 'https://1k2a51u50g.execute-api.eu-central-1.amazonaws.com/dev',
-    import: 'https://iof9qtnvlj.execute-api.eu-central-1.amazonaws.com/dev',
-    bff: 'https://1k2a51u50g.execute-api.eu-central-1.amazonaws.com/dev',
-    cart: 'https://1k2a51u50g.execute-api.eu-central-1.amazonaws.com/dev',
+    product: 'https://ryb23pa0hh.execute-api.eu-central-1.amazonaws.com/dev',
+    order: 'https://ryb23pa0hh.execute-api.eu-central-1.amazonaws.com/dev',
+    import: 'https://jl4ms5yh77.execute-api.eu-central-1.amazonaws.com/dev',
+    bff: 'https://ryb23pa0hh.execute-api.eu-central-1.amazonaws.com/dev',
+    cart: 'https://ryb23pa0hh.execute-api.eu-central-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
